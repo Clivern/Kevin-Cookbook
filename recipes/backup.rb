29af56b584faf898827605220ab0349b1cfc868e
Clivern <hello@clivern.com>
2018-05-11 21:04:09 +0200

    Add all recipes

---
+++ recipes/backup.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: kevin-cookbook
+# Recipe:: backup
+#
+# Copyright:: 2018, Clivern, All Rights Reserved.
+
---
+++ recipes/basic.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: kevin-cookbook
+# Recipe:: basic
+#
+# Copyright:: 2018, Clivern, All Rights Reserved.
+
