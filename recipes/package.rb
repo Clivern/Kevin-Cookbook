973e616fcc3237a570f10e7ea4111918e56cf1da
Clivern <hello@clivern.com>
2018-05-11 21:44:42 +0200

    Add nginx and packages recipes

--- recipes/package.rb
+++ recipes/package.rb
@@ -4,3 +4,38 @@
 #
 # Copyright:: 2018, Clivern, All Rights Reserved.
 
+
+node['package']['required'].each do |package|
+    log "Installing Package #{package}"
+    package "#{package}" do
+      action :install
+    end
+end
+
+node['package']['install'].each do |package|
+    log "Installing Package #{package}"
+    package "#{package}" do
+      action :install
+    end
+end
+
+node['package']['upgrade'].each do |package|
+    log "Upgrading Package #{package}"
+    package "#{package}" do
+      action :upgrade
+    end
+end
+
+node['package']['remove'].each do |package|
+    log "Removing Package #{package}"
+    package "#{package}" do
+      action :remove
+    end
+end
+
+node['package']['purge'].each do |package|
+    log "Purging Package #{package}"
+    package "#{package}" do
+      action :purge
+    end
+end
