8de20435824731b1a982b68e81e84812bce78f5e
Clivern <hello@clivern.com>
2018-05-11 22:06:49 +0200

    add pip recipe

--- recipes/pip.rb
+++ recipes/pip.rb
@@ -4,3 +4,9 @@
 #
 # Copyright:: 2018, Clivern, All Rights Reserved.
 
+log "recipe::pip"
+log "Installing Pip for Python Version #{node['python']['version']}"
+
+package "python#{node['python']['version']}-pip" do
+    action :install
+end
