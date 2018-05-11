1c8441b24a6d7f2c095a8c9fde5d67fb3d5c888a
Clivern <hello@clivern.com>
2018-05-11 21:28:55 +0200

    basic apt commands added

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: kevin-cookbook
+# Attributes:: default
+#
+# Copyright:: 2018, Clivern, All Rights Reserved.
+
+default['basic']['update'] = true
+default['basic']['upgrade'] = false
+default['basic']['autoremove'] = false
--- recipes/basic.rb
+++ recipes/basic.rb
@@ -4,3 +4,21 @@
 #
 # Copyright:: 2018, Clivern, All Rights Reserved.
 
+
+execute "apt-get update" do
+    command "apt-get update"
+    live_stream true
+    only_if { node['basic']['update'] }
+end
+
+execute "apt autoremove" do
+    command "apt -y autoremove"
+    live_stream true
+    only_if { node['basic']['autoremove'] }
+end
+
+execute "apt-get upgrade" do
+    command "apt-get -y upgrade"
+    live_stream true
+    only_if { node['basic']['upgrade'] }
+end
